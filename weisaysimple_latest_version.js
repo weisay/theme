8560558fa8b6e19925ee263dfa3cc8c807ecfdc8
weisay <wxwvv@163.com>
2014-07-21 16:51:15 +0800

    Create weisaysimple_latest_version.js
    1

---
+++ weisaysimple_latest_version.js
@@ -0,0 +1,3 @@
+var weisaytheme_latest_version = "1.62";
+var downloand_add = "http://www.weisay.com/blog/weiimg/article/weisaysimple.zip"
+var author_add = "http://www.weisay.com/blog/wordpress-theme-weisay-simple.html"
