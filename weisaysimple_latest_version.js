9b8e206722f53d91836829075b3a7076aecb90d3
weisay <wxwvv@163.com>
2015-02-02 18:14:03 +0800

    Update weisaysimple_latest_version.js

--- weisaysimple_latest_version.js
+++ weisaysimple_latest_version.js
@@ -1,3 +1,3 @@
-var weisaytheme_latest_version = "1.62";
-var downloand_add = "http://www.weisay.com/blog/weiimg/article/weisaysimple.zip"
+var weisaytheme_latest_version = "2.0";
+var downloand_add = "http://pan.baidu.com/s/1ntl2WzR"
 var author_add = "http://www.weisay.com/blog/wordpress-theme-weisay-simple.html"
