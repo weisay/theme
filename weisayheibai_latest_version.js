eef12b5c872982cf71342fd4e7c12eedc6978d0d
weisay <wxwvv@163.com>
2015-02-11 17:21:27 +0800

    Update weisayheibai_latest_version.js

--- weisayheibai_latest_version.js
+++ weisayheibai_latest_version.js
@@ -1,3 +1,3 @@
 var weisaytheme_latest_version = "1.3";
-var downloand_add = "http://pan.baidu.com/s/1mgtCJPI"
+var downloand_add = "http://pan.baidu.com/s/1qW8xFfm"
 var author_add = "http://www.weisay.com/blog/wordpress-theme-weisay-heibai.html"
